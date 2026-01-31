25d51e3320350d6b5df2c0ef38208ca3f701dff6
manuelhernandezleon <47336108+manuelhernandezleon@users.noreply.github.com>
2026-01-31 14:39:50 -0600

    eliminé los bordes

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: ComponentProps<"article">) {
     <article
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6",
+        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl py-6",
         className
       )}
       {...props}
